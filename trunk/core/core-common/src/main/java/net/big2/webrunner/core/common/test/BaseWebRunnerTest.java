a7c65abdf87a0967b453da59f91c3319e2ba2752
kctang@big2.net <kctang@big2.net@690b9fd4-4354-78d3-ada4-fadd404ede06>
2012-04-24 12:31:24 +0000

    Added BaseWebRunnerTest, disable Jetty directory listing, enable default servlet handling & log Maven test output to console.

---
+++ trunk/core/core-common/src/main/java/net/big2/webrunner/core/common/test/BaseWebRunnerTest.java
@@ -0,0 +1,14 @@
+package net.big2.webrunner.core.common.test;
+
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:webrunner.xml"})
+public abstract class BaseWebRunnerTest {
+    protected Logger log = LoggerFactory.getLogger(BaseWebRunnerTest.class);
+
+}
