8f743dd7fdee9ec4700ee1aec36d89dc56825892
kctang@big2.net <kctang@big2.net@690b9fd4-4354-78d3-ada4-fadd404ede06>
2012-06-05 15:43:53 +0000

    Updated demo modules

--- demo/demo-web/src/main/java/net/big2/webrunner/demo/web/Demo.java
+++ demo/demo-web/src/main/java/net/big2/webrunner/demo/web/Demo.java
@@ -1,14 +1,14 @@
 package net.big2.webrunner.demo.web;
 
+import model.demo.jpa.User;
 import net.big2.webrunner.core.common.WrmConfiguration;
-import model.Person;
-import repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import repository.demo.jpa.user.UserRepository;
 
 import java.util.Map;
 
@@ -18,14 +18,14 @@ public class Demo {
     ApplicationContext context;
 
     @Autowired
-    private PersonRepository personRepository;
+    private UserRepository userRepository;
 
     @RequestMapping("list")
     Model ok(Model model) {
         Map<String, WrmConfiguration> configMap = context.getBeansOfType(WrmConfiguration.class);
         model.addAttribute(configMap);
 
-        model.addAttribute("personList", personRepository.findAll());
+        model.addAttribute("userList", userRepository.findAll());
 
         return model;
     }
@@ -33,10 +33,10 @@ public class Demo {
     @RequestMapping("add")
     @ResponseBody
     String add() {
-        Person person = new Person();
-        person.setName("bab " + System.currentTimeMillis());
-
-        personRepository.save(person);
+        User user = new User();
+        user.setUsername("bob");
+        user.setPassword("secret");
+        userRepository.save(user);
 
         return "Added";
     }
